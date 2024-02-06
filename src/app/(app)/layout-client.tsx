35d78d6f20b704da5c6876e57d68ee88713c03dc
RK Anik <79830581+encoderit-anik@users.noreply.github.com>
2024-02-06 10:52:06 +0600

    Fix overflow issue

--- src/app/(app)/layout-client.tsx
+++ src/app/(app)/layout-client.tsx
@@ -56,49 +56,47 @@ export default function AppLayoutClient({ children }: TProps) {
 	)
 
 	return (
-		<Layout className='h-screen !bg-[url(/img/bg2.jpg)] !bg-cover !bg-top !bg-no-repeat'>
-			<div className='absolute inset-0 z-0 bg-black/90 backdrop-blur-md'></div>
-			<Layout.Sider
-				{...{ collapsed }}
-				width={250}
-				className='!bg-white/[0.09] !backdrop-blur-md border-r border-white border-opacity-10 !sticky !top-0'
-				onCollapse={(v) => setCollapsed(v)}
-			>
-				<div className='h-16 flex items-center px-3 text-lg'>R.K.</div>
-				<div className='p-1'>
+		<div className='h-screen !bg-[url(/img/bg3.jpg)] !bg-cover !bg-top !bg-no-repeat flex flex-col overflow-hidden'>
+			<div className='flex flex-1 overflow-hidden'>
+				<div className='absolute inset-0 z-0 bg-black/90 backdrop-blur-md'></div>
+				<Layout.Sider
+					{...{ collapsed }}
+					width={250}
+					className='!bg-white/[0.09] !backdrop-blur-md border-r border-white border-opacity-10 !sticky !top-0'
+					onCollapse={(v) => setCollapsed(v)}
+				>
+					<div className='h-16 flex items-center px-3 text-lg'>R.K.</div>
 					<Menu
 						theme='dark'
 						mode='inline'
-						className='!bg-transparent'
+						className='!bg-transparent !p-1'
 						defaultSelectedKeys={['Home']}
 						items={items}
 					/>
+				</Layout.Sider>
+				<div className='relative flex-1 flex flex-col '>
+					<div className='flex-1 z-10 relative overflow-auto'>
+						<Layout.Header className='!p-0 m-3 !bg-white/[0.09] !backdrop-blur-md !rounded-lg !sticky !top-3 z-50 border border-white border-opacity-5'>
+							<Button
+								type='text'
+								icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+								onClick={() => setCollapsed(!collapsed)}
+								style={{
+									fontSize: '16px',
+									width: 64,
+									height: 64,
+								}}
+							/>
+						</Layout.Header>
+						<Layout.Content className='px-3'>
+							<div className='h-[1200px] '>{children}</div>
+						</Layout.Content>
+					</div>
 				</div>
-			</Layout.Sider>
-			<div className='flex-1 z-10 relative overflow-auto'>
-				<Layout.Header
-					className={cn(
-						'!p-0 m-3 !bg-white/[0.09] !backdrop-blur-md !rounded-lg !sticky !top-3 z-50',
-					)}
-				>
-					<Button
-						type='text'
-						icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-						onClick={() => setCollapsed(!collapsed)}
-						style={{
-							fontSize: '16px',
-							width: 64,
-							height: 64,
-						}}
-					/>
-				</Layout.Header>
-				<Layout.Content className='px-3'>
-					<div className='h-[1200px] '>{children}</div>
-				</Layout.Content>
-				<Layout.Footer className='h-6 !py-0 flex items-center !px-2 mt-3 !bg-white/[0.09] !backdrop-blur-md !sticky !bottom-0 z-50'>
-					Footer
-				</Layout.Footer>
 			</div>
-		</Layout>
+			<div className='h-6 flex items-center px-2 !bg-white/[0.09] !backdrop-blur-md flex-none border-t border-white border-opacity-10'>
+				Footer
+			</div>
+		</div>
 	)
 }
